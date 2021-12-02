4624e63e76bfe89bb61a552a567525b1cfd987d5
Ruilin Zoe <angel2007zoe@gmail.com>
2021-12-01 23:16:17 -0800

    Assignment 4 minor update

--- assignment4/SynchronizedLinkedList.java
+++ assignment4/SynchronizedLinkedList.java
@@ -4,6 +4,10 @@ public class SynchronizedLinkedList<T>{
     private LinkedList<Integer> list;
     private Object lock;
 
+    public SynchronizedLinkedList(){
+        this.lock=new Object();
+        this.list=new LinkedList<>();
+    }
     public void addAtPosition(int index, int element){
         synchronized (lock){
             list.add(index,element);
