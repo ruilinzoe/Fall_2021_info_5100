11d3b20e04ecd49dc02c33bb10e3d42e03b80f44
Ruilin Zoe <angel2007zoe@gmail.com>
2021-12-10 02:30:26 -0800

    Assignment 6

--- assignment6/DrivingLicenseApplication.java
+++ assignment6/DrivingLicenseApplication.java
@@ -1,4 +1,45 @@
 package assignment6;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
+
+
 public class DrivingLicenseApplication {
+
+    public static void main(String[] args) {
+        System.out.print("Please enter date of birth in YYYY-MM-DD: ");
+        Scanner scanner=new Scanner(System.in);
+        String input=scanner.nextLine();
+        scanner.close();
+
+        try{
+            LocalDate dob = LocalDate.parse(input);
+            getAge(dob);
+        }
+        catch (Exception e) {
+            System.out.println("Your DOB entered is invalid");
+        }
+    }
+
+    // Returns age given the date of birth
+    public static int getAge(LocalDate dob) {
+        LocalDate curDate = LocalDate.now();
+        int age=Period.between(dob, curDate).getYears();
+        try {
+            if (age>16){
+                throw new Exception ();
+            }
+            if (age<=16)
+            System.out.println("The age of the applicant is " + age+ " which is too early to apply for a driving license");
+        }
+        catch (Exception e) {
+            System.out.println("The age of the applicant is " + age+ " which is eligible to apply for a driving license");
+        }
+        finally {
+            System.out.println("Thank you!");
+        }
+        return age;
+    }
+
 }
