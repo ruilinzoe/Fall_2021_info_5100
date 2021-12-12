a4927fb3748e9e5bc31deb5eb03b179e284d95be
Ruilin Zoe <angel2007zoe@gmail.com>
2021-12-11 18:42:15 -0800

    Assignment 6

--- assignment6/Main.java
+++ assignment6/Main.java
@@ -1,4 +1,43 @@
 package assignment6;
 
+
+import java.lang.reflect.Array;
+import java.util.*;
+
 public class Main {
+    public static void main(String[] args) {
+        Student student1=new Student(01,"Mark",3.5, new Date(2018,01,01));
+        Student student2=new Student(02,"Liz",3.1, new Date(1998,03,01));
+        Student student3=new Student(03,"Tom",2.5, new Date(1990,02,01));
+        Student student4=new Student(04,"Tim",3.7, new Date(1989,12,01));
+        Student student5=new Student(05,"Dam",3.3, new Date(1997,06,01));
+
+        List<Student> list=new ArrayList<> (Arrays.asList(student1,student2,student3,student4,student5));
+
+        for(Student s: list){
+            System.out.println(s.toString());
+        }
+
+        Collections.sort(list, new Student.NameComparator());
+        System.out.printf("----");
+        System.out.println("ascending order by name");
+        for (Student s:list){
+            System.out.println(s.toString());
+        }
+
+        Collections.sort(list, new Student.GpaComparator());
+        System.out.printf("----");
+        System.out.println("descending order by GPA");
+        for (Student s:list){
+            System.out.println(s.toString());
+        }
+
+
+        Collections.sort(list, new Student.DateOfBirthComparator());
+        System.out.printf("---");
+        System.out.println("Birthday order");
+        for (Student s: list){
+            System.out.println(s.toString());
+        }
+    }
 }
--- assignment6/Student.java
+++ assignment6/Student.java
@@ -1,5 +1,6 @@
 package assignment6;
 
+import java.util.Comparator;
 import java.util.Date;
 
 public class Student {
@@ -7,4 +8,76 @@ public class Student {
     private String name;
     private double gpa;
     private Date dateOfBirth;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Student(int id, String name, double gpa, Date dateOfBirth){
+        this.id=id;
+        this.name=name;
+        this.gpa=gpa;
+        this.dateOfBirth=dateOfBirth;
+    }
+
+    static class NameComparator implements Comparator <Student>{
+        @Override
+        public int compare(Student o1, Student o2) {
+            return o1.getName().compareTo(o2.getName());
+        }
+    }
+
+    static class GpaComparator implements Comparator <Student>{
+
+        @Override
+        public int compare(Student o1, Student o2) {
+            return Double.compare(o2.getGpa(),o1.getGpa());
+        }
+    }
+
+    static class DateOfBirthComparator implements Comparator <Student>{
+
+        @Override
+        public int compare(Student o1, Student o2) {
+            return o1.getDateOfBirth().compareTo(o2.getDateOfBirth());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gpa=" + gpa +
+                ", dateOfBirth=" + dateOfBirth +
+                '}';
+    }
 }
